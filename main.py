fcafdaf08cebeb09c6dec518ffa06053572c7c59
HAmzaaider <kohat3067@gmail.com>
2026-08-17 10:20:54 +0500

    delete api done

--- main.py
+++ main.py
@@ -173,3 +173,20 @@ def delete_patient(patient_id: str):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message': 'patient deleted'})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    # load data
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
+
+
